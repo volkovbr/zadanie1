61a9eaed0275fd7bc255d6db4c48ac4c6f081a70
volkov test <hlip1989@mail.ru>
2023-05-23 16:59:39 +0300

    testng

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,83 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+public class CashbackHackServiceTest {
+    @Test
+    public void сash500() {
+        CashbackHackService cashbackHackerService = new CashbackHackService();
+        int amount = 500;
+
+
+        int actual = cashbackHackerService.remain(amount);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void сash999() {
+        CashbackHackService cashbackHackerService = new CashbackHackService();
+        int amount = 999;
+
+
+        int actual = cashbackHackerService.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void сash1000() {
+        CashbackHackService cashbackHackerService = new CashbackHackService();
+        int amount = 1000;
+
+
+        int actual = cashbackHackerService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void сash1() {
+        CashbackHackService cashbackHackerService = new CashbackHackService();
+        int amount = 1;
+
+
+        int actual = cashbackHackerService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void сash1001() {
+        CashbackHackService cashbackHackerService = new CashbackHackService();
+        int amount = 1001;
+
+
+        int actual = cashbackHackerService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void сash50() {
+        CashbackHackService cashbackHackerService = new CashbackHackService();
+        int amount = 50;
+
+
+        int actual = cashbackHackerService.remain(amount);
+        int expected = 950;
+
+        assertEquals(actual, expected);
+    }
+
+}
+
+
+
